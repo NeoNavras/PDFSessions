63cc02b40ae58636998747253504d54897d84708
Adrian Green <git@adriangreen.com.au>
2019-02-05 15:57:21 +1000

    Uses JSON to save session data (instead of split string). Adds 'Remove All' menu item. If doc already open, does not update page location. Misc improvements.

--- savesessions.js
+++ savesessions.js
@@ -1,90 +1,105 @@
 /*
- save session is a java script for Acrobat Reader
- based on save tabs by Andrey Kartashov found on
+  save session is a java script for Acrobat Reader
+  based on save tabs by Andrey Kartashov found on
+  https://stackoverflow.com/questions/12689154/adobe-acrobat-reader-tabs-saving-and-autoloading
+  Put it in $HOME/.adobe/Acrobat/9.0/JavaScripts (or in
+  the equivalent program files folder under Windows,
+  %appdata%/adobe/acrobat/Privileged/DC/JavaScripts)
+  and it will automatically
+  be loaded.
 */
 
-var delim = '|';
 var parentMenu = "Window";
-var openMenu = "&Open Session"
-var deleteMenu = "&Delete Session"
+var openMenu = "&Open Session";
+var deleteMenu = "&Delete Session";
+var deleteAll = "&Remove All Sessions";
 
 // Dialog Definition 
 var oDlg = {
-	strName: "SessionName", 
-	initialize: function(dialog) { 
-					dialog.load({"usnm":this.strName}); 
-				} ,
-    commit: function(dialog) { 
-              var data = dialog.store();
-              this.strName = data["usnm"];
-		    },
-    description: { name: "Test Dialog", elements: 
-					[	
-						{ type: "view", elements: 
-							[
-								{ name: "Enter your name:", type: "static_text", },
-								{ item_id: "usnm", type: "edit_text", char_width: 40 },
-								{ type: "ok_cancel", },
-							]
-						},
-					] 
-				}	
+  strName: "SessionName",
+  initialize: function (dialog) {
+    dialog.load({"usnm": this.strName});
+  },
+  commit: function (dialog) {
+    var data = dialog.store();
+    this.strName = data["usnm"];
+  },
+  description: {
+    name: "Session Dialog", elements:
+      [
+        {
+          type: "view", elements:
+            [
+              {name: "Enter your name:", type: "static_text",},
+              {item_id: "usnm", type: "edit_text", char_width: 40},
+              {type: "ok_cancel",},
+            ]
+        },
+      ]
+  }
 };
 
-function AddSession(name){	
-	
- 	app.addMenuItem({
-	  cName: "open"+name,
-	  cUser: name,
-	  cParent: openMenu,
-	  cExec: "LoadTabs(\""+ name +"\");"
-	});
-	
-	app.addMenuItem({
-	  cName: "del"+name,
-	  cUser: name,
-	  cParent: deleteMenu,
-	  cExec: "DeleteTab(\"" + name +"\");"
-	}); 
-	
-	
+/*
+Loads named session into menus
+*/
+function AddSession(name) {
+  app.addMenuItem({
+    cName: "open" + name,
+    cUser: name,
+    cParent: openMenu,
+    cExec: "LoadTabs(\"" + name + "\");"
+  });
+
+  app.addMenuItem({
+    cName: "del" + name,
+    cUser: name,
+    cParent: deleteMenu,
+    cExec: "DeleteTab(\"" + name + "\");"
+  });
 }
 
 /*
  Loading Saved Tabs
 */
+function LoadTabs(name) {
+  var session = getSession(name);
+  if (session == null || session == []) {
 
-
-function LoadTabs(session) {
-
-  if (global.tabs_opened == null) {
     return;
   }
+  var d = trustedActiveDocs();
+  var paths = [];
+  for (var i = 0; i < d.length; i++) {
+    paths.push(d[i].path)
+  }
 
-  var flat = global.tabs_opened[session].split(delim);
-  for (i = 0; i < flat.length; i += 2) {
+  for (doc in session) {
     try {
-      app.openDoc(flat[i]);
-      app.execMenuItem("FirstPage");
-      for (ii = 0; ii < flat[i + 1]; ++ii) {
-        app.execMenuItem("NextPage");
+      var d = app.openDoc(session[doc].path);
+      // if a session doc already open, do not change page
+      if(-1 == paths.indexOf(session[doc].path)) {
+        d.pageNum = session[doc].pageNum;
       }
-    } catch (ee) {
-      app.alert("Error while opening the requested document.\n" + flat[i], 3);
+    } catch (e) {
+      console.println('LoadTabs: ' + e);
     }
   }
 }
 
-function DeleteTab(name){
-	
-	if(app.alert("Are you shure you wnat to delete " + name + " ?", 2, 2, "Submit Validation") == 4){
-		app.hideMenuItem("open"+name);
-		app.hideMenuItem("del"+name);
-		delete global.tabs_opened[name];
-		global.setPersistent("tabs_opened", true);
-	}
+function DeleteTab(name, warn) {
+  var proceed = true;
+  if (undefined !== warn && warn) {
+    if (app.alert("Are you sure you want to delete " + name + " ?", 2, 2, "Submit Validation") != 4) {
+      proceed = false;
+    }
+  }
+  if (proceed) {
+    app.hideMenuItem("open" + name);
+    app.hideMenuItem("del" + name);
+    delete global.tabs_opened[name];
+    global.setPersistent("tabs_opened", true);
+  }
 }
-
 
 /*
  Function with trusted section returning opened documents
@@ -96,53 +111,78 @@ trustedActiveDocs = app.trustedFunction(function () {
   return d;
 })
 
+function getSession(name) {
+  var result = [];
+  try {
+    if (global.tabs_opened[name]) {
+      result = JSON.parse(global.tabs_opened[name]);
+    }
+  } catch (ee) {
+    console.println('getSession: ' + ee);
+  }
+
+  return result;
+}
+
 /*
  Saving Tabs that are opened
 */
 function SaveSession() {
-	
- 
-  var d = trustedActiveDocs();
-  var tabs = '';
-  
-  if(app.execDialog(oDlg) == "ok") {
-	  
-	  var nRslt = 4
-	  var already_exists = oDlg.strName in global.tabs_opened;
-	  if(already_exists)
-		nRslt = app.alert("A session with that name already exists...\n\n" + "Do you want to continue?", 2, 2, "Submit Validation");
-	  
-	  console.println(nRslt);
-	  
-	  if(nRslt == 4){
-	  
-		  var session = oDlg.strName;
-		  
-		  if(d.length == 0){
-			  app.alert("No documents opened to save session!", 3);
-		  }
-	  
-			for (var i = 0; i < d.length; i++) {
-				if (i > 0)
-				  tabs += delim;
-				//    app.alert(d[i].path+"------"+d[i].pageNum,3);
-				tabs += d[i].path;
-				tabs += delim;
-				tabs += d[i].pageNum;
-			}
-		  
-		  	global.tabs_opened[session] = tabs
-			global.setPersistent("tabs_opened", true);
-			if(!already_exists)
-				AddSession(session, tabs) 	
-		}
-  }
-} 
-  
 
-if(global.tabs_opened == null){
-	 global.tabs_opened = []
-	 global.setPersistent("tabs_opened", true);
+  var d = trustedActiveDocs();
+
+  if (d.length == 0) {
+    app.alert("No documents opened to save session", 3);
+
+    return;
+  }
+
+  if (app.execDialog(oDlg) == "ok") {
+
+    var nRslt = 4
+    var savedSession = getSession(oDlg.strName);
+    console.println('SaveSession: ' + savedSession);
+    var already_exists = (savedSession.length > 0);
+    if (already_exists) {
+      nRslt = app.alert("A session with name " + oDlg.strName + " already exists...\n\n" + "Do you want to continue?", 2, 2, "Submit Validation");
+    }
+
+    if (nRslt == 4) {
+      var session = oDlg.strName;
+      docs = [];
+
+      for (var i = 0; i < d.length; i++) {
+        docs.push(
+          {
+            'path': d[i].path,
+            'pageNum': d[i].pageNum
+          }
+        )
+      }
+      json = JSON.stringify(docs);
+      console.println(json);
+      global.tabs_opened[session] = json;
+      global.setPersistent("tabs_opened", true);
+
+      AddSession(session)
+    }
+  }
+}
+
+/*
+ Remove all saved sessions
+*/
+function RemoveAllSessions() {
+  if (4 == app.alert("Are you sure you want to delete all sessions?", 2, 2, "Submit Validation")) {
+    for (index in global.tabs_opened) {
+      DeleteTab(index, false);
+    }
+  }
+}
+
+if (global.tabs_opened == null) {
+  global.tabs_opened = []
+  global.setPersistent("tabs_opened", true);
 }
 
 app.addMenuItem({
@@ -167,6 +207,12 @@ app.addSubMenu({
   cParent: parentMenu,
 });
 
+app.addMenuItem({
+  cName: deleteAll,
+  cParent: parentMenu,
+  cExec: "RemoveAllSessions();"
+});
+
 for (key in global.tabs_opened) {
-	AddSession(key);
+  AddSession(key);
 }
